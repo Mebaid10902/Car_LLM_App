ca0f435501c60eed508736c4d5b39f31261f1a49
mahmoud ebaid <135147097+Mebaid10902@users.noreply.github.com>
2025-09-20 11:57:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-AZURE_OPENAI_KEY="6gONwn1USiPyhh0LX8IKuHzXbR9n3E1T2y0eapF3XNhIIjT82u7YJQQJ99BHACHYHv6XJ3w3AAABACOGrSyw"
+AZURE_OPENAI_KEY="Enter your key"
 AZURE_DEPLOYMENT_NAME="gpt-4o-mini-ME174"
-AZURE_ENDPOINT="https://orionopenai2-techtest.openai.azure.com/"
+AZURE_ENDPOINT="Enter your end point"
 GMAIL_ADDRESS="Enter your mail" # Donot remove double quotes
 GMAIL_APP_PASSWORD="Enter your app_password" # Donot remove double quotes
 MAX_RETRIES = 3
